f29fef2e25b3c48fdcb24d3c66d388a703147510
hanifatulnadiva <161436287+hanifatulnadiva@users.noreply.github.com>
2024-05-28 15:02:43 +0700

    membuat class pelajar  turunan ddari class orang

--- solveMultiwaris.cpp
+++ solveMultiwaris.cpp
@@ -15,3 +15,10 @@ class pekerja : public orang{
             cout <<"pekerja dibuat\n" << endl;
         }
 };
+
+class pelajar : public orang{
+    public:
+        pelajar(int pUmur) : orang(pUmur){
+            cout <<"pelajar dibuat\n" << endl;
+        }
+};
