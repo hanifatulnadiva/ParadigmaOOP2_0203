f02387ecca0cfbe1c026d1dcbc40114ae4683285
hanifatulnadiva <161436287+hanifatulnadiva@users.noreply.github.com>
2024-05-28 15:37:38 +0700

    membuat fungsi main

--- noPewarisan.cpp
+++ noPewarisan.cpp
@@ -14,3 +14,9 @@ class derivedClass : public baseClass{
             cout << "hallo saya function dari derived class";
         }
 };
+
+int main(){
+    derivedClass a;
+    a.perkenalan();
+    return 0;
+}
