6a1d0b40ae43ba5dddf9ae0c540fa9a5ebe4d3c4
hanifatulnadiva <161436287+hanifatulnadiva@users.noreply.github.com>
2024-05-28 15:26:09 +0700

    membuat multiwaris ditambah dengan  virtual

---
+++ multiwaris.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+
+class orang {
+    public:
+        int umur;
+        orang(int pUmur) : umur(pUmur){
+            cout <<"orang dibuat dengan umur" << umur <<"\n" << endl;
+        }
+};
+
+class pekerja : virtual public orang{
+    public:
+        pekerja(int pUmur) : orang(pUmur){
+            cout <<"pekerja dibuat\n" << endl;
+        }
+};
+
+class pelajar : virtual public orang{
+    public:
+        pelajar(int pUmur) : orang(pUmur){
+            cout <<"pelajar dibuat\n" << endl;
+        }
+};
+
+class Budi : public pekerja, public pelajar{
+    public: 
+        Budi(int pUmur) : pekerja (pUmur), pelajar(pUmur), orang(pUmur) { // hal ini dapat dilakukan jika menggunakan virtual
+            cout <<"Budi dibuat\n" << endl;
+        }
+};
+
+int main(){
+    Budi a(12);
+    return 0;
+}
